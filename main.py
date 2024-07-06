5714b8c9841d31bee6d973522a870b260192eac7
Gopal <gopalsognigbe@gmail.com>
2024-07-06 13:25:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,58 @@
-def afficher_information_personne(nom,age,taille=0):
-    print(f"votre nom est {nom}, vous avez {age} ans l'an prochain vous aurez {age + 1} ans")
-    if age == 1 or age ==2:
-        print('vous etes un bebe')
-    elif age < 10 :
-        print("vous etes mineur")
-    elif age == 17:
-        print('Vous etes presque majeur')
-    elif age >= 12 and age < 18:
-        print('vous etes adolescent')
-    elif age == 18:
-        print('vous etes tout juste majeur')
-    elif age > 60:
-        print('vous etes senior')
-    elif age > 18:
-        print('vous etes majeur')
-    # afficher la taille 
-    if taille != 0:
-        print('votre taille est :'+str(taille)+' m')
+
+#escalier/scales
+def escalier(taille,nombre_de_marche):
+    import turtle 
+    t=turtle.Turtle()
+
+    for i in range(0,int(nombre_de_marche)):
+        t.forward(int(taille))
+        t.left(90)
+        t.forward(int(taille))
+        t.right(90)
+        #taille=taille-5
+        taille_int=int(taille)
+        taille_int-=10
+    t.forward(taille)
+    turtle.done()
+
+#carre/square
+def carre (taille):
+    import turtle
+    t=turtle.Turtle()
+    for i in range(4):
+        t.forward(taille)
+        t.left(90)
+    turtle.done()
 
 
-def demander_nom ():
-    nom= input('entrer le nom: ')
-    while nom=='':
-        nom= input('entrer le nom: ')
-    return nom
+#carres/squares
+def carres(taille_depart, nb_carre):
+    import turtle
+    t=turtle.Turtle()
+    for i in range(nb_carre):
+        taille=(i+1)*taille_depart
+        for i in range(4):
+            t.forward(taille)
+            t.left(90)
+    turtle.done()
 
 
-def demander_age(nom_personne):
-    age_int=0
-    while     age_int==0:
-        age=input(f'votre age {nom_personne} :')
-        try:
-            age_int= int(age)
-        except:
-            print('Erreur:vous devez rentrer un nombre ')
-    return age_int
-""" 
-#demander nom
-nom=demander_nom()
-nom1=demander_nom()
+print(""" Jeu de la tortue 
+      1-creation de marches (escaliers)
+      2-creation d'un carre 
+      3-creation de plusieurs carres imbriqués 
+      """)
 
-#demander age
-age=demander_age(nom)
-age1=demander_age(nom1)
 
-#afficher information
-afficher_information_personne(nom,age)1
-afficher_information_personne(nom1,age1)
-"""
-for i in range(0,3):
-    nom='personne '+ str(i +1)
-    age=demander_age(nom)
-    afficher_information_personne(nom,age,)
+choix=input("faites votre choix :")
+if int(choix )==1:
+    taille=input("creation d'excaliers: veuillez entrer la taille de marches : ")
+    nombre_demarche=input("le nombre de marche ")
+    escalier(taille,nombre_demarche)
+if int(choix)==2:
+    taille_carre=input("création de carré: veuillez entrer la taille de chaque coté :")
+    carre(int(taille_carre))
+if int(choix)==3:
+    nb_carres=input("creation de plusieurs carrés imbriqués : veuillez entrer le nombre de carrés : ")
+    taille_carres=input('entrez la taille coté du premier carré :')
+    carres(int(taille_carres),int(nb_carres))
